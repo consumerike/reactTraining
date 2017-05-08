0c5a4a90d36696164a6d1bddf577120644d9f10d
Ikechi <consumerike@gmail.com>
2017-05-08 14:46:44 -0500

    added battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,9 +1,33 @@
 var React = require('react');
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(id, username) {
+    this.setState(function{
+      var newState = {};
+    });
+  }
   render() {
     return (
+      <div className="row">
+        {!playerOneName && <PlayerInput 
+          id='playerOne'
+          label='Player One'
+          onSubmit={this.handleSubmit }
+        />}
 
+        {!playerTowName && <PlayerInput />}
+      </div>
     )
   }
 }
